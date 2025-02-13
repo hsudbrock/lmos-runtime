655316e70d9f2c4eaf9274fbf8235ace440383df
Henning Sudbrock <henning.sudbrock@telekom.de>
2025-02-13 09:22:08 +0100

    agent

--- lmos-runtime-service/src/main/kotlin/org/eclipse/lmos/runtime/service/inbound/agent/RuntimeAgent.kt
+++ lmos-runtime-service/src/main/kotlin/org/eclipse/lmos/runtime/service/inbound/agent/RuntimeAgent.kt
@@ -3,20 +3,25 @@ package org.eclipse.lmos.runtime.service.inbound.agent
 import ai.ancf.lmos.wot.protocol.LMOSContext
 import ai.ancf.lmos.wot.protocol.LMOSThingType
 import ai.ancf.lmos.wot.reflection.annotations.Context
+import ai.ancf.lmos.wot.reflection.annotations.Property
 import ai.ancf.lmos.wot.reflection.annotations.Thing
 import ai.ancf.lmos.wot.reflection.annotations.VersionInfo
+import kotlinx.coroutines.flow.MutableStateFlow
 import org.springframework.stereotype.Component
 
 @Thing(
     id = "runtime-agent",
     title = "Runtime Agent",
     description = "The runtime agent",
-    type = LMOSThingType.AGENT)
+    type = LMOSThingType.AGENT,
+)
 @Context(
     prefix = LMOSContext.prefix,
-    url = LMOSContext.url
+    url = LMOSContext.url,
 )
 @VersionInfo(instance = "1.0.0")
 @Component
 class RuntimeAgent {
+    @Property(title = "current mood", readOnly = true)
+    val observableProperty: MutableStateFlow<String> = MutableStateFlow("bad")
 }
